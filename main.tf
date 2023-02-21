c6310fed33b42c12346550924648a4e394a3b08d
Andrew Chasin <andrewchasin@google.com>
2023-02-21 15:56:34 -0500

    add tagging test and iterator with count

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 resource "google_tags_tag_key" "key" {
-  parent = "organizations/123456789"
-  short_name = "excludeFromOrgPolicy"
-  description = "For keyname resources."
+  parent = var.parent
+  short_name = var.key_name
+  description = var.key_description
 }
 
-#TODO: iterator for values 
 resource "google_tags_tag_value" "value" {
-    for_each = var.values_list
+    count = length(var.values_list)
     parent = "tagKeys/${google_tags_tag_key.key.name}"
-    short_name = "domainRestrictedSharing"
+    short_name = var.values_list["${count.index}"]
 }
